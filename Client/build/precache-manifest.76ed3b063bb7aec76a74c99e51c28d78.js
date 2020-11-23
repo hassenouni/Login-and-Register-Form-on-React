3f157e06ea2d4b75d31888aa403340ae1abff123
Hassen.Ouni <hassenouni@users.noreply.github.com>
2020-11-23 17:04:21 +0100

    Add files via upload
    deploying in Heroku

---
+++ Client/build/precache-manifest.76ed3b063bb7aec76a74c99e51c28d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf87f862552562ad8198bf495b3f8d17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a137327ab361891d062",
+    "url": "/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "f389b83c91e0b8891c37",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a137327ab361891d062",
+    "url": "/static/js/2.545efcb9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.545efcb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f389b83c91e0b8891c37",
+    "url": "/static/js/main.c63dfead.chunk.js"
+  },
+  {
+    "revision": "7b26885d37676037f748",
+    "url": "/static/js/runtime-main.2bb0a87c.js"
+  }
+]);
